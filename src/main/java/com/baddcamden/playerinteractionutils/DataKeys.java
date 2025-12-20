3b18fc4c4475f0fef2ff4271ea1f383a49a6a300
BaddCamden <84610525+Camden-Tech@users.noreply.github.com>
2025-12-20 00:20:37 -0600

    Add distinct spawn keys for player tagging

--- src/main/java/com/baddcamden/playerinteractionutils/DataKeys.java
+++ src/main/java/com/baddcamden/playerinteractionutils/DataKeys.java
@@ -8,8 +8,9 @@ public class DataKeys {
     public final NamespacedKey blockGrownFromPlayer;
     public final NamespacedKey blockTransformedFromPlayer;
 
-    public final NamespacedKey entitySpawnCause;
-    public final NamespacedKey entitySpawnPlayer;
+    public final NamespacedKey eggSpawnPlayer;
+    public final NamespacedKey spawnEggSpawnPlayer;
+    public final NamespacedKey breedingSpawnPlayer;
     public final NamespacedKey lastHitBy;
     public final NamespacedKey lastHitAt;
     public final NamespacedKey damageByPlayer;
@@ -18,10 +19,23 @@ public class DataKeys {
         this.blockOwner = new NamespacedKey(plugin, "block-owner");
         this.blockGrownFromPlayer = new NamespacedKey(plugin, "block-grown-from-player");
         this.blockTransformedFromPlayer = new NamespacedKey(plugin, "block-transformed-from-player");
-        this.entitySpawnCause = new NamespacedKey(plugin, "spawn-cause");
-        this.entitySpawnPlayer = new NamespacedKey(plugin, "spawn-player");
+        this.eggSpawnPlayer = new NamespacedKey(plugin, "egg-spawn-player");
+        this.spawnEggSpawnPlayer = new NamespacedKey(plugin, "spawn-egg-spawn-player");
+        this.breedingSpawnPlayer = new NamespacedKey(plugin, "breeding-spawn-player");
         this.lastHitBy = new NamespacedKey(plugin, "last-hit-by");
         this.lastHitAt = new NamespacedKey(plugin, "last-hit-at");
         this.damageByPlayer = new NamespacedKey(plugin, "damage-by-player");
+    }
+
+    public DataKeys(String namespace) {
+        this.blockOwner = new NamespacedKey(namespace, "block-owner");
+        this.blockGrownFromPlayer = new NamespacedKey(namespace, "block-grown-from-player");
+        this.blockTransformedFromPlayer = new NamespacedKey(namespace, "block-transformed-from-player");
+        this.eggSpawnPlayer = new NamespacedKey(namespace, "egg-spawn-player");
+        this.spawnEggSpawnPlayer = new NamespacedKey(namespace, "spawn-egg-spawn-player");
+        this.breedingSpawnPlayer = new NamespacedKey(namespace, "breeding-spawn-player");
+        this.lastHitBy = new NamespacedKey(namespace, "last-hit-by");
+        this.lastHitAt = new NamespacedKey(namespace, "last-hit-at");
+        this.damageByPlayer = new NamespacedKey(namespace, "damage-by-player");
     }
 }
